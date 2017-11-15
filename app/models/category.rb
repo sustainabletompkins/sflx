ff7799246fb3463c43eae24c4f048144c6529abf
Alex C <alex@swidjit.com>
2017-11-15 13:34:32 -0500

    tags on edit page

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ActiveRecord::Base
   has_many :lists
-  has_many :listings, through: :lists, class_name: 'Listing'
+  has_many :listings, -> { distinct }, through: :lists, class_name: 'Listing'
 end
