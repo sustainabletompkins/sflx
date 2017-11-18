cf2a117ada105fc83e1772a2dcc0ba35a1cd57bc
Alex C <alex@swidjit.com>
2017-11-18 16:38:19 -0500

    additions to listing page

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,7 @@
 class List < ActiveRecord::Base
   has_and_belongs_to_many :listings
   belongs_to :category
+  belongs_to :user
   scope :inactive, lambda { where('active = FALSE') }
-  scope :active, lambda { where('active = TRUE') } 
+  scope :active, lambda { where('active = TRUE') }
 end
