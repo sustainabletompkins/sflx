ef64fb300023833b4607a0c461f54acb6e046059
Alex C <alex@swidjit.com>
2017-11-23 13:17:53 -0500

    show url link in listing

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -101,6 +101,7 @@ class ListingsController < ApplicationController
     @listings.each do |l|
       arr = [l.title, l.latitude, l.longitude]
       html = "<div class='info_content'><h3>#{l.title}</h3><p>#{l.description}</p>"
+      html += "<div class='address'><span>#{l.address}</span>"
       html += "<div class='website'><a href='#{l.website}'>#{l.website}</a></div></div>" if l.website.present?
       html += "<div class='phone'><a href='tel:+1#{l.phone.gsub(/\D/, "")}'>#{l.phone}</a></div></div>" if l.phone.present?
       html += "<div class='email'><a href='mailto:#{l.email}' target='_blank'>#{l.email}</a></div></div>" if l.email.present?
