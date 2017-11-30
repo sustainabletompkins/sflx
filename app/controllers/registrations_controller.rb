c878a6dab6b1c229a615d59caf96c30b72ab244f
Alex C <alex@swidjit.com>
2017-11-30 11:11:29 -0500

    change redirect path after registration to valid url

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    '/map/category/Sustainable%20Economy' # Or :prefix_to_your_route
+    '/map/category/sustainable-economy' # Or :prefix_to_your_route
   end
 end
